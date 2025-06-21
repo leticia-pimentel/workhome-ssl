333f0ef1bc521ee7d16745672e94ef416c219bef
leticia-pimentel <leticiaparagao@gmail.com>
2025-06-20 23:27:57 -0300

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+## Comandos Git simulados
+
+- git init
+- git add hello_world.cpp
+- git commit -m "Adiciona programa Hello World"
+- git push
